b667ec8fcf1a240179eb87ed99729740a77ee8c0
Johnsony0 <johnsony@bu.edu>
2023-11-13 17:52:50 -0500

    made default sort by descending order (critical -> high -> medium -> ...)

--- CLI/k8s-scan/vulnerabilities_func.py
+++ CLI/k8s-scan/vulnerabilities_func.py
@@ -20,11 +20,18 @@ def evaluate(driver_vul, version, list):
         {"version":version},routing = RoutingControl.READ, database = "neo4j"
     )
     df = pd.DataFrame(records,columns=['NAME','INSTALLED','FIXED-IN','TYPE','VULNERABILITY','SEVERITY'])
-    series = df['SEVERITY'].value_counts().to_string()
-    print(series)
+    #print data such as total vulnerabilities, and their distribution
     print("total vulnerabilities in version",version, "is",len(records), "with distribution as followed: ")
     print(df['SEVERITY'].value_counts().to_string())
     print("or in terms of percentages:")
     print(df['SEVERITY'].value_counts(normalize=True).to_string())
+    #print if list is true
     if(list):
+        #create mapping to custom sort pandas dataframe
+        mapping = {
+            'Critical':0, 'High':1, 'Medium':2, 'Low':3, 'Negligible':4, 'Unknown':5
+        }
+        df['sorting'] = df['SEVERITY'].apply(lambda state: mapping[state])
+        df.sort_values(by=['sorting'],inplace=True,ignore_index=True)
+        df.drop('sorting',axis=1,inplace=True)
         print(tabulate(df,headers='keys',tablefmt='psql'))
