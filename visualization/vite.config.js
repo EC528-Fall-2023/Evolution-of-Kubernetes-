b1dfb7667cfdfd39f9f372c785b16a642eddfc27
JeffC25 <jeff@yerf.dev>
2023-11-27 14:01:43 -0500

    rebuild

--- visualization/vite.config.js
+++ visualization/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://ec528-fall-2023.github.io/Evolution-of-Kubernetes-/",
+  base: "/Evolution-of-Kubernetes-/",
 })
