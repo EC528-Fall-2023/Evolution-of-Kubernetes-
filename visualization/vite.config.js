25b28674ac829024c67a57cc3a21f86cc6f1639e
JeffC25 <jeff@yerf.dev>
2023-11-27 13:57:31 -0500

    remove whitespace from viteconfig base

--- visualization/vite.config.js
+++ visualization/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: " https://EC528-Fall-2023.github.io/Evolution-of-Kubernetes-/",
+  base: "https://ec528-fall-2023.github.io/Evolution-of-Kubernetes-/",
 })
