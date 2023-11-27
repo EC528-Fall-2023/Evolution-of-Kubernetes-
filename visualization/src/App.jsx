428f2a4e727617ed6974f323fee7a6ca17da7c89
JeffC25 <jeff@yerf.dev>
2023-11-27 00:08:39 -0500

    integrate grype data into UI

--- visualization/src/App.jsx
+++ visualization/src/App.jsx
@@ -1,25 +1,28 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import vulnerabilities from './data/vulnerabilities.json';
 
 function App() {
 
   // JUST FOR TESTING
-  const testdata = {
-    'Version A': { name: 'Version A', before: 4000, after: 2400, },
-    'Version B': { name: 'Version B', before: 5432, after: 1324, },
-    'Version C': { name: 'Version C', before: 4000, after: 1432, },
-    'Version D': { name: 'Version D', before: 1432, after: 3333, },
-    'Version E': { name: 'Version E', before: 4321, after: 4431, },
-    'Version F': { name: 'Version F', before: 4000, after: 1223, },
-    'Version G': { name: 'Version G', before: 4000, after: 1223, },
-    'Version H': { name: 'Version H', before: 4000, after: 2400, },
-    'Version I': { name: 'Version I', before: 5432, after: 1324, },
-    'Version J': { name: 'Version J', before: 4000, after: 1432, },
-    'Version K': { name: 'Version K', before: 1432, after: 3333, },
-    'Version L': { name: 'Version L', before: 3232, after: 4431, },
-    'Version M': { name: 'Version M', before: 4000, after: 1223, },
-    'Version N': { name: 'Version N', before: 4000, after: 1223, }
-  }
+  // const testdata = {
+  //   'Version A': { name: 'Version A', low: 2432, high: 2400, },
+  //   'Version B': { name: 'Version B', low: 5432, high: 1324, },
+  //   'Version C': { name: 'Version C', low: 4000, high: 1432, },
+  //   'Version D': { name: 'Version D', low: 1432, high: 3333, },
+  //   'Version E': { name: 'Version E', low: 4321, high: 4431, },
+  //   'Version F': { name: 'Version F', low: 4000, high: 1223, },
+  //   'Version G': { name: 'Version G', low: 1233, high: 1223, },
+  //   'Version H': { name: 'Version H', low: 4000, high: 2400, },
+  //   'Version I': { name: 'Version I', low: 5432, high: 1324, },
+  //   'Version J': { name: 'Version J', low: 4000, high: 1432, },
+  //   'Version K': { name: 'Version K', low: 1432, high: 3333, },
+  //   'Version L': { name: 'Version L', low: 3232, high: 4431, },
+  //   'Version M': { name: 'Version M', low: 4000, high: 1223, },
+  //   'Version N': { name: 'Version N', low: 4000, high: 1223, }
+  // }
+
+  const testdata = vulnerabilities
 
   const [versions, setVersions] = useState([])
   const [selected, setSelected] = useState([])
@@ -28,7 +31,8 @@ function App() {
   // NOTE:
   // We're gonna need to either fetch a list of all versions or hard-code it
   useEffect(() => {
-    setVersions(['Version A', 'Version B', 'Version C', 'Version D', 'Version E', 'Version F', 'Version G', 'Version H', 'Version I', 'Version J', 'Version K', 'Version L', 'Version M', 'Version N'])
+    // setVersions(['Version A', 'Version B', 'Version C', 'Version D', 'Version E', 'Version F', 'Version G', 'Version H', 'Version I', 'Version J', 'Version K', 'Version L', 'Version M', 'Version N'])
+    setVersions(Object.keys(testdata))
     console.log("fetched versions")
   }, [])
 
@@ -36,7 +40,7 @@ function App() {
     // NOTE:
     // when we fetch from neo4j, make sure to handle this with async
     setChartData(
-      selected.map((i) => testdata[i])
+      selected.map(i => ({ name: i, ...testdata[i] }))
     )
     console.log(chartData)
   }, [selected])
@@ -51,45 +55,51 @@ function App() {
   // }
 
   return (
+    
     <div className="h-screen flex flex-row p-10">
       <div className="flex-grow overflow-x-auto">
-        <ResponsiveContainer width="100%" height="100%">
+        
+        <h1 className="absolute text-center font-semibold top-4 right-1/2">Kubernetes Vulnerabilities by Version</h1>
+        <ResponsiveContainer >
+          
           <BarChart
-            width={500}
-            height={300}
+            className="p-5"
             data={chartData}
-            margin={{
-              top: 5,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}
           >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
+            <XAxis dataKey="name" angle={-45} textAnchor="end" height={80} />
+            <YAxis label={{ value: 'Vulnerabilities', angle: -90, position: 'insideLeft' }} />
             <Tooltip />
             <Legend />
-            <Bar dataKey="after" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
-            <Bar dataKey="before" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
+            <Bar dataKey="Negligible" fill="olivedrab" stackId="a" activeBar={<Rectangle fill="lightskyblue" />} />
+            <Bar dataKey="Low" fill="gold" stackId="a" activeBar={<Rectangle fill="lightskyblue" />} />
+            <Bar dataKey="Medium" fill="orange" stackId="a" activeBar={<Rectangle fill="lightskyblue" />} />
+            <Bar dataKey="High" fill="orangered" stackId="a" activeBar={<Rectangle fill="lightskyblue" />} />
+            <Bar dataKey="Critical" fill="red" stackId="a" activeBar={<Rectangle fill="lightskyblue" />} />
+            <Bar dataKey="Unknown" fill="gray" stackId="a" activeBar={<Rectangle fill="lightskyblue" />} />
           </BarChart>
         </ResponsiveContainer>
       </div>
-      <ul className="flex flex-shrink flex-col h-full overflow-y-scroll ">
-        {versions.map((i) => <button
-          key={i}
-          onClick={() => {
-            if (selected.includes(i)) {
-              setSelected(selected.filter((element) => element !== i))
-            } else {
-              setSelected(selected.concat(i))
-            }
-          }}
-          className={`border text-left p-1 border-neutral-300 w-40 ${selected.includes(i) ? "bg-blue-300" : "bg-neutral-200"}`}
-        >
-          {i}
-        </button>)}
-      </ul>
+      <div className="flex flex-col w-32">
+        <button onClick={() => setSelected([])} className="bg-neutral-200 p-1 border border-neutral-300 hover:bg-sky-200 w-full">Clear</button>
+        <button onClick={() => setSelected(versions)} className="bg-neutral-200 p-1 border border-neutral-300 hover:bg-sky-200 w-full">Select All</button>
+        <ul className="flex flex-shrink flex-col h-full overflow-y-scroll ">
+        
+          {versions.map((i) => <button
+            key={i}
+            onClick={() => {
+              if (selected.includes(i)) {
+                setSelected((selected.filter((element) => element !== i)).sort())
+              } else {
+                setSelected((selected.concat(i)).sort())
+              }
+            }}
+            className={`border text-left p-1 border-neutral-300 ${selected.includes(i) ? "bg-sky-300 hover:bg-sky-200" : "bg-neutral-200 hover:bg-sky-100"}`}
+          >
+            {i}
+          </button>)}
+        </ul>
+      </div>
     </div>
   )
 }
