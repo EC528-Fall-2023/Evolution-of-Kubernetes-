9af9da811ff6b38660633eafbdd8ca3035503890
JeffC25 <jeff@yerf.dev>
2023-11-27 13:26:40 -0500

    add base to vite config

--- visualization/vite.config.js
+++ visualization/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: " https://EC528-Fall-2023.github.io/Evolution-of-Kubernetes-/",
 })
