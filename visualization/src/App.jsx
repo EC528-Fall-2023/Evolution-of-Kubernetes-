596cf7f8665a5b8b375c945bbd7299625a5e6ca7
JeffC25 <jeff@yerf.dev>
2023-11-27 20:58:16 -0500

    add semantic version sorting -- still need to fix prerelease tag sorting

--- visualization/src/App.jsx
+++ visualization/src/App.jsx
@@ -8,10 +8,27 @@ function App() {
   const [selected, setSelected] = useState([])
   const [chartData, setChartData] = useState([])
 
+  const compareSemVer = (a, b) => {
+ 
+    // 1. Split the strings into their parts.
+    const a1 = a.split('.');
+    const b1 = b.split('.');    // 2. Contingency in case there's a 4th or 5th version
+    const len = Math.min(a1.length, b1.length);    // 3. Look through each version number and compare.
+    for (let i = 0; i < len; i++) {
+        const a2 = +a1[ i ] || 0;
+        const b2 = +b1[ i ] || 0;
+        
+        if (a2 !== b2) {
+            return a2 > b2 ? 1 : -1;        
+        }    }
+    
+    // 4. We hit this if the all checked versions so far are equal
+    //
+    return b1.length - a1.length;};
   // NOTE: We're gonna need to either fetch a list of all versions or hard-code it
   useEffect(() => {
     // setVersions(['Version A', 'Version B', 'Version C', 'Version D', 'Version E', 'Version F', 'Version G', 'Version H', 'Version I', 'Version J', 'Version K', 'Version L', 'Version M', 'Version N'])
-    setVersions(Object.keys(testdata))
+    setVersions((Object.keys(testdata)).sort(compareSemVer))
     console.log("fetched versions")
   }, [])
 
